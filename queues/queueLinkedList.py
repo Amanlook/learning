7108a6ab0300b9e20c9d4f74c22f799a32234649
Aman Sharma <110001067+Amanlook@users.noreply.github.com>
2026-06-07 19:53:16 +0530

    Implement SinglyLinkedList and QueueLinkedList classes with basic operations

---
+++ queues/queueLinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value=None):
+        
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+class QueueLinkedList:
+
+    def __init__(self):
+        self.linkedList = LinkedList()
+    
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' '.join(values)
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.linkedList.head is None:
+            self.linkedList.head = newNode
+            self.linkedList.tail = newNode
+        else:
+            self.linkedList.tail.next = newNode
+            self.linkedList.tail = newNode
+        
+    def isempty(self):
+        return self.linkedList.head is None
+
+    def dequeue(self):
+        if  self.isempty():
+            return "No Queue"
+        
+        tempNode = self.linkedList.head
+        if self.linkedList.head == self.linkedList.tail:
+            self.linkedList.head = self.linkedList.tail = None
+        else:
+            self.linkedList.head = self.linkedList.head.next
+         
+        return tempNode
+    
+    def peek(self):
+        if  self.isempty():
+            return "No Queue"
+        return self.linkedList.head
+
+    def delete(self):
+        self.linkedList.head = self.linkedList.tail = None
+
+        
--- trees/binarytree.py
+++ trees/binarytree.py
@@ -1,4 +1,5 @@
 
+from queues.queueLinkedList import QueueLinkedList as queue
 
 class TreeNode:
 
@@ -13,8 +14,16 @@ newBT = TreeNode("Drinks")
 leftchild = TreeNode("Hot")
 rightChild = TreeNode("Cold")
 
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+
 newBT.left = leftchild
 newBT.right = rightChild
+
+leftchild.left = tea
+leftchild.right = coffee
+
+# ---------------- DFS
 
 def preOrderTraversal(rootNode: TreeNode):
     if not rootNode:
@@ -25,4 +34,72 @@ def preOrderTraversal(rootNode: TreeNode):
     preOrderTraversal(rootNode.left)
     preOrderTraversal(rootNode.right)
 
-preOrderTraversal(newBT)
+# preOrderTraversal(newBT)
+
+def inOrderTraversal(rootNode: TreeNode):
+    if not rootNode:
+        return 
+    
+    inOrderTraversal(rootNode.left)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.right)
+
+# inOrderTraversal(newBT)
+
+def postOderTraversal(rootNode: TreeNode):
+
+    if not rootNode:
+        return 
+    
+    postOderTraversal(rootNode.left)
+    postOderTraversal(rootNode.right)
+    print(rootNode.data)
+
+# postOderTraversal(newBT)
+
+# ---------------- BFS
+
+def levelOrderTraversal(rootNode: TreeNode):
+
+    if not rootNode:
+        return
+    
+    custom_queue = queue()
+    custom_queue.enqueue(rootNode)
+    while not(custom_queue.isempty()):
+        root = custom_queue.dequeue()
+        print(root.value.data)
+
+        if root.value.left is not None:
+            custom_queue.enqueue(root.value.left)
+
+        if root.value.right is not None:
+            custom_queue.enqueue(root.value.right)
+
+# levelOrderTraversal(newBT)
+
+def searchBT(rootNode: TreeNode, value):
+    # Using level Order Traversal
+    if not rootNode:
+        return "The BT does not exist"
+     
+    custom_queue = queue()
+    custom_queue.enqueue(rootNode)
+    while not(custom_queue.isempty()):
+        root = custom_queue.dequeue()
+        
+        if root.value.data == value:
+            return True
+
+        if root.value.left is not None:
+            custom_queue.enqueue(root.value.left)
+
+        if root.value.right is not None:
+            custom_queue.enqueue(root.value.right)
+    
+    return False
+
+
+# print(searchBT(newBT, "Tea"))
+
+ 
